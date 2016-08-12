39bfb5e518acaf036020432f1caacfd28aeaca1b
suni <suni@suni>
2016-08-12 21:46:57 +0200

    zadaci 11.08.2016.

--- src/zadaci_11_08_2016/TrenutniDatumIVrijeme.java
+++ src/zadaci_11_08_2016/TrenutniDatumIVrijeme.java
@@ -13,55 +13,24 @@ public class TrenutniDatumIVrijeme {
 
 	}
 
-	//dobijamo trenutno vrijeme
+	// dobijamo trenutno vrijeme
 	public static String getCurrentTime() {
 		long ukupnoSekundi = System.currentTimeMillis() / 1000;
 		long sekundi = ukupnoSekundi % 60;
 		long minuta = ukupnoSekundi / 60 % 60;
-		//+2 je da bi izravnali vremenske zone
-		long sati = ((ukupnoSekundi / 3600)+2) % 24;
+		// +2 je da bi izravnali vremenske zone
+		long sati = ((ukupnoSekundi / 3600) + 2) % 24;
 		return sati + ":" + minuta + ":" + sekundi;
 	}
 
-	//doijamo trenutni datum
+	// dobijamo trenutni datum
 	public static String getCurrentDate() {
 		GregorianCalendar gc = new GregorianCalendar();
 		int dan = gc.get(GregorianCalendar.DAY_OF_MONTH);
 		int mjesec = gc.get(GregorianCalendar.MONTH);
 		int godina = gc.get(GregorianCalendar.YEAR);
-		return dan + ". " + kojiMjesec(mjesec) + ", " + godina;
-	}
-
-	// provjerava koji je mjesec
-	public static String kojiMjesec(int mjesec) {
-
-		switch (mjesec) {
-		case 0:
-			return "Januar";
-		case 1:
-			return "Februar";
-		case 2:
-			return "Mart";
-		case 3:
-			return "April";
-		case 4:
-			return "Maj";
-		case 5:
-			return "Jun";
-		case 6:
-			return "Jul";
-		case 7:
-			return "August";
-		case 8:
-			return "Septembar";
-		case 9:
-			return "Octobar";
-		case 10:
-			return "Novembar";
-		case 11:
-			return "Decembar";
-
-		}
-		return "Pogresan unos!";
+		String[] kojiMjesec = { "Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "August", "Septembar",
+				"Oktobar", "Novembar", "Decembar" };
+		return dan + ". " + kojiMjesec[mjesec] + ", " + godina;
 	}
 }
