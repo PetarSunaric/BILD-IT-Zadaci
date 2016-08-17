c3775b98f81afe42ecdbc95972a698a098b4e386
suni <suni@suni>
2016-08-17 11:08:29 +0200

    zadaci 16.08.2016.

--- src/zadaci_16_08_2016/TacanDatumIVrijeme.java
+++ src/zadaci_16_08_2016/TacanDatumIVrijeme.java
@@ -1,15 +1,20 @@
 package zadaci_16_08_2016;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TacanDatumIVrijeme {
 
 	public static void main(String[] args) {
-		// koristimo klasu date koja sadrzi trenutacni datum i vrijeme
-		Date date = new Date();
-		// metoda toString nam vraca sve to.
-		System.out.println(date.toString());
+		System.out.println("Trenutni datum i vrijeme: " + getCurrentTime());
 
 	}
 
+	public static String getCurrentTime() {
+		// koristimo klasu date koja sadrzi trenutacni datum i vrijeme
+		Date date = new Date();
+		// metoda toString nam vraca sve to a usput i formatiramo sta i kako
+		// zelimo da dobijemo
+		return new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(date).toString();
+	}
 }
