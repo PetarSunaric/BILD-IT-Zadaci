60531513e293d7615b303578bff31cc616cc14af
suni <suni@suni>
2016-08-25 23:57:04 +0200

    zadaci 24.08.2016.

--- src/zadaci_24_08_2016/MersennePrime.java
+++ src/zadaci_24_08_2016/MersennePrime.java
@@ -6,18 +6,17 @@ public class MersennePrime {
 
 	public static void main(String[] args) {
 
-		// number for testing and counter for count prime numbers
-		int num = 2, count = 0;
+		// number for testing
+		int num = 2;
 		System.out.println("p		2^p-1\n");
 		do {
 			if (isPrime(num)) { // if number is prime write him and he's
 								// mersenne form
 				System.out.print(num + "		" + new BigInteger("2").pow(num).subtract(BigInteger.ONE));
 				System.out.println();
-				count++; // increase count of prime numbers by 1
 			}
 			num++; // increase number that is tested
-		} while (count < 100);
+		} while (num <= 100);
 
 	}
 
