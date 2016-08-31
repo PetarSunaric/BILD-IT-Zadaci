08351ceecd96a7bf715996895d12d30ac0278f25
suni <suni@suni>
2016-08-31 22:39:34 +0200

    zadaci 30.08.2016.

--- src/zadaci_30_08_2016/RemoveTexT.java
+++ src/zadaci_30_08_2016/RemoveTexT.java
@@ -20,29 +20,19 @@ public class RemoveTexT {
 			System.out.println(args[1] + " does not exist.");
 			System.out.println(2);
 		}
-		// creating a help file that stores all the same as entered file but
-		// without entered word
-		File helpFile = new File("helpfile.txt");
-		FileWriter fw = new FileWriter(helpFile);
-		Scanner output = new Scanner(filename);
-		while (output.hasNextLine()) {
-			String str = output.nextLine();// taking a line
-			str.replaceAll(args[0], "");// removes all words same as entered
-			fw.write(str);// write a line in helpFile
-		}
-		// copy from help file to entered file
-		Scanner output2 = new Scanner(helpFile);
-		FileWriter fw2 = new FileWriter(filename);
-		while (output2.hasNextLine()) {
-			String str = output.nextLine();
-			fw2.write(str + "\n");
 
+		Scanner output = new Scanner(filename);
+		String str = "";
+		while (output.hasNextLine()) {
+			str += output.nextLine() + "\n";// taking a whole file content
 		}
+		str.replaceAll(args[0], "");// removes all words same as entered
+
+		FileWriter fw = new FileWriter(filename);
+		fw.write(str + "\n");// writes string into file
 
 		fw.close();
 		output.close();
-		fw2.close();
-		output2.close();
 	}
 
 }
